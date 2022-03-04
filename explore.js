c0be68d322f78bda0689882af239441412355d9b
Sobuj <sobujkumar424@gmail.com>
2022-03-04 22:31:12 +0600

    explore

---
+++ explore.js
@@ -0,0 +1,8 @@
+const name = 10;
+const student = {name: 'Sobuj', friend:'Akash', job: 'Business',
+    makeMovie: function(name){
+        console.log(name);
+    }
+};
+student.makeMovie('Din raat');
+// document.getElementById('name');
