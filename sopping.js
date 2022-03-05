f6f685f113823edfbe923ce0983b62dea9571f7c
Sobuj <sobujkumar424@gmail.com>
2022-03-05 22:12:23 +0600

    sopping-2

--- sopping.js
+++ sopping.js
@@ -1,14 +1,23 @@
+const displayLocalCart = () => {
+    const cart = getCart();
+    for(const name in cart){
+        displayProduct(name);
+    }
+}
+//
 const addItem = () => {
     const nameFild = document.getElementById('product-name');
     const name = nameFild.value;
     nameFild.value = '';
+    if(!name){
+        return;
+    }
     //call display in the ui
     displayProduct(name);
     //call add to local storage
-    addProductToCart(name);
-
-    
+    addProductToCart(name); 
 }
+//
 const displayProduct = name => {
     const ul = document.getElementById('products');
     const li = document.createElement('li');
@@ -30,7 +39,18 @@ const getCart = () => {
 //
 const addProductToCart = name => {
     const cart = getCart();
-    cart[name] = 1;
+    if(cart [name]){
+        cart[name] = cart[name]+1;
+    }
+    else{
+        cart[name] = 1;
+    }
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 }
+//
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+}
+displayLocalCart();
